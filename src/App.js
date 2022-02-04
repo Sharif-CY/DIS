a36fbb16a505464ad4f2b47f21384a0239ee31e3
sharif.choudry <sharif.choudry@gmail.com>
2022-02-04 20:04:52 +0100

    first commit added project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welkom op pijn
         </a>
       </header>
     </div>
